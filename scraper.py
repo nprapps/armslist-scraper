dda268d1ecdbde685d4fa1360663d14df0409fe7
Brittany Mayes <brmayes16@gmail.com>
2016-06-13 17:32:07 -0400

    Added stop function

--- scraper.py
+++ scraper.py
@@ -10,26 +10,29 @@ ARMSLIST_URL = 'http://www.armslist.com/classifieds/search'
 STOP_DATETIME = datetime(2016, 6, 13, 0, 0, 0)
 
 def scrape(stop_datetime):
-    param_arguments = [('location', 'florida'), ('category', 'guns'), ('posttype', '3'), ('page', 1)]
+    param_arguments = [('location', 'usa'), ('category', 'guns'), ('posttype', '3'), ('page', 1)]
     params = OrderedDict(param_arguments)
-    response = requests.get(ARMSLIST_URL, params=params)
-    print(response.url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    container = soup.find(id='bootstrap-overrides')
-    divs = container.find_all('div')
-    soup.find_all('div', position='absolute')
-    for div in divs:
-        href = div.get('href')
-        if href:
-            table = div.find('table')
-            second_td = div.find_all('td')[1]
-            third_div = second_td.find_all('div')[2]
-            junk, date_string = third_div.string.strip().split(', ')
-            parsed_date = parse(date_string)
-            if parsed_date < stop_datetime:
-                break
-            print(parsed_date)
-
+    stop = False
+    while not stop:
+        response = requests.get(ARMSLIST_URL, params=params)
+        print(response.url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        container = soup.find(id='bootstrap-overrides')
+        divs = container.find_all('div')
+        soup.find_all('div', position='absolute')
+        for div in divs:
+            href = div.get('href')
+            if href:
+                table = div.find('table')
+                second_td = div.find_all('td')[1]
+                third_div = second_td.find_all('div')[2]
+                junk, date_string = third_div.string.strip().split(', ')
+                parsed_date = parse(date_string)
+                if parsed_date < stop_datetime:
+                    stop = True
+                    break
+                print('{0}: {1}'.format(parsed_date, href))
+        params['page'] += 1
 
 if __name__ == '__main__':
     scrape(STOP_DATETIME)
