f0c84430890853020efff69332e25ea8a69641f9
Brittany Mayes <brmayes16@gmail.com>
2016-06-13 14:35:41 -0400

    Scrapes html for divs with an href

--- scraper.py
+++ scraper.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python
 import requests
 
+from bs4 import BeautifulSoup
+from collections import OrderedDict
+
+ARMSLIST_URL = 'http://www.armslist.com/classifieds/search'
+
 def scrape():
-    print('hello world')
+    param_arguments = [('location', 'florida'), ('category', 'all'), ('posttype', '7')]
+    params = OrderedDict(param_arguments)
+    response = requests.get(ARMSLIST_URL, params=params)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    container = soup.find(id='bootstrap-overrides')
+    divs = container.find_all('div')
+    for div in divs:
+        href = div.get('href')
+        if href:
+            print(div)
+        
+
 
 if __name__ == '__main__':
     scrape()
