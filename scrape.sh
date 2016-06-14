ab7a1336bc3a6eda2c6d963ae5f5f5928faf9b89
David Eads <davideads@gmail.com>
2016-06-14 17:05:28 -0400

    add header row to listings.csv

--- scrape.sh
+++ scrape.sh
@@ -2,4 +2,5 @@
 
 ./states.py | parallel --results cache/states ./scrape_index.py {} > cache/index.csv
 
-csvcut -c 1 cache/index.csv | parallel ./scrape_listing.py {} > cache/listings.csv
+echo "url,post_id,title,listed_date,price,location,city,state,description,registered,category,manufacturer,caliber,action,firearm_type,party,img" > cache/listings.csv
+csvcut -c 1 cache/index.csv | parallel ./scrape_listing.py {} >> cache/listings.csv
