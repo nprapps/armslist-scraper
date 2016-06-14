75275b342e38342f3e78e7dda77a7e5efe69f23c
David Eads <davideads@gmail.com>
2016-06-14 15:42:16 -0400

    add some csv cut magic to parallelize the listing scraper

--- scrape.sh
+++ scrape.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 ./states.py | parallel --results cache/states ./scrape_index.py {} > cache/index.csv
+
+csvcut -c 1 cache/index.csv | parallel ./scrape_listing.py {} > cache/listings.csv
