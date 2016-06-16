1c95498c05e6dc97a5a0bd64b0515c95edf0bb54
David Eads <davideads@gmail.com>
2016-06-16 07:33:00 -0400

    correct call to deduper

--- scrape.sh
+++ scrape.sh
@@ -2,7 +2,7 @@
 
 ./states.py | parallel --results cache/states ./scrape_index.py {} > cache/index.csv
 
-./dedupe_csv.py
+./dedupe_index.py
 
 echo "url,post_id,title,listed_date,price,location,city,state,description,registered,category,manufacturer,caliber,action,firearm_type,party,img" > cache/listings.csv
 csvcut -c 1 cache/index-deduped.csv | parallel ./scrape_listing.py {} >> cache/listings.csv
