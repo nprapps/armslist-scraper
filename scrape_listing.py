de319d526dd6eccff3d877ec7a50919271e7afbf
David Eads <davideads@gmail.com>
2016-10-01 12:56:02 -0400

    Update scrape_listing.py

--- scrape_listing.py
+++ scrape_listing.py
@@ -17,19 +17,7 @@ def scrape_listing(url):
         url,
         listing.post_id,
         listing.title,
-        listing.listed_date,
-        listing.price,
-        listing.location,
-        listing.city,
-        listing.state,
-        listing.description,
-        listing.registered,
-        listing.category,
-        listing.manufacturer,
-        listing.caliber,
-        listing.action,
-        listing.firearm_type,
-        listing.party,
+        # ...
         listing.img
     ])
 
